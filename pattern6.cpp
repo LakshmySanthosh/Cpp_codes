36ebeb65128c1c0af08d3dca8841fd8238db6c62
Lakshmy <121610033+LakshmySanthosh@users.noreply.github.com>
2023-09-27 12:20:52 +0530

    Patterns

---
+++ pattern6.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+/*
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+*/
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=n-i-1;j>0;j--)
+        {
+            cout<<"  ";
+        }
+        for(int j=0;j<=i;j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
